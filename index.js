cd644601ba1a9230d36325a006573f44df6a0ffa
joao vieira <john.collins.vieira@gmail.com>
2023-07-01 12:43:56 -0400

    complete math lab

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(x, y){
+    return x + y 
+};
+function subtract(x, y){
+    return x - y 
+};
+function multiply(x, y){
+    return x * y
+};
+function divide(x, y){
+    return x / y
+};
+function increment(number){
+return (number += 1)
+}
+function decrement(number){
+    return (number -= 1)
+}
+function makeInt(n) {
+  return parseInt(n, 10)
+}function preserveDecimal(n) {
+    return parseFloat(n, 10)
+}
